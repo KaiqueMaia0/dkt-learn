d3a791ee52ff1751d8483ddb85f7313f21e514bb
KaiqueMaia0 <kmaia5041@gmail.com>
2025-05-22 00:40:06 -0300

    Edit Inicial-page

--- dkt-learn-front/src/components/Inicial.jsx
+++ dkt-learn-front/src/components/Inicial.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 import "./Inicial.css";
 import dktlogo from "../components/images/dktlogo.png";
 import perfil from "../components/images/perfil.png";
+import sino from "../components/images/sino.png";
 
 function Inicial() {
+  const [showDropdown, setShowDropdown] = React.useState(false);
+
   return(
     <div className="app">
       <header className="header" padding-top="64px">
@@ -14,14 +17,44 @@ function Inicial() {
           </button>
         </div>
         <nav>
-          <a href="#sobre">Sobre</a>
           <a href="#materiais">Materiais</a>
           <a href="#comunidade">Comunidade</a>
-          <a href="#perfil">
-            <img src="" alt="perfil" />  
-          </a>
+          <a href="#dashboard">Dashboard</a>
+          <button style={{ background: 'none', border: 'none', padding: 0, cursor: 'pointer' }}>
+            <a href="#notificacoes">
+              <img src={sino} alt="notificações" style={{ width: '30px', height: 'auto', background: 'none' }}/>  
+            </a>
+          </button>
+          <div className="perfil-dropdown"
+            onMouseEnter={() => setShowDropdown(true)}
+            onMouseLeave={() => setShowDropdown(false)}
+            style={{ display: 'inline-block', position: 'relative' }}>
+            <button style={{ background: 'none', border: 'none', padding: 0, cursor: 'pointer' }}>
+              <a href="#perfil">
+                <img src={perfil} alt="perfil" style={{ width: '45px', height: 'auto', background: 'none' }}/>  
+              </a>
+            </button>
+            {showDropdown && (
+              <div className="dropdown-menu">
+                <Link to="/perfil">Meu Perfil</Link>
+                <Link to="/meus-cursos">Meus Cursos</Link>
+                <Link to="/favoritos">Favoritos</Link>
+                <Link to="/suporte">Suporte</Link>
+                <Link to="/configuracoes">Configurações</Link>
+                <Link to="/sair">Sair</Link>
+              </div>
+            )}
+          </div>  
         </nav>  
       </header>
+      <main>
+        <section className="hero">
+          <div classname="informaçoes" style={{paddingTop: '100px'}}>
+            <h1 style={{width: '550px', height: 'auto', borderRadius: '12px'}}>Bem vindo à DKT Learn!</h1>
+            <p>Uma plataforma para aprender Calculo e Física de um jeito totalmente diferente!</p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
